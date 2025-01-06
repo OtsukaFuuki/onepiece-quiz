c4bb0fb4a416660b8ba20258f6a08cf68083789d
OtsukaFuuki <ys.training231129@gmail.com>
2025-01-06 22:16:54 +0900

    表示を確認

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
 "use client";
 
-import Home from "./components/Home";
+import React from "react";
+import { useRouter } from "next/navigation";
 
-export default function Page() {
-  return <Home />;
-}
+const Home: React.FC = () => {
+  const router = useRouter();
+
+  const handleStartQuiz = () => {
+    // クイズに遷移
+    router.push("/quiz");
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen top-bg1">
+      <div className="w-full max-w-md p-4">
+        <button
+          onClick={handleStartQuiz}
+          className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          クイズスタート
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
